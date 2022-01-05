3e5b9c6778da7e51026bcde5ceb7879a041764ba
Richard Tillies <rtillies@gmail.com>
2022-01-05 11:50:29 -0500

    Create museum class and spec files

---
+++ spec/museum_spec.rb
@@ -0,0 +1,16 @@
+require './lib/exhibit'
+require './lib/patron'
+require './lib/museum'
+
+RSpec.describe Museum do
+  describe 'Iteration 2' do
+    before :each do
+    end
+
+    it 'exists' do
+    end
+
+    it 'has attributes' do
+    end
+  end
+end

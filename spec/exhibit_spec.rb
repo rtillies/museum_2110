6f88d858ed365697635e8a29dc0047d0d8e769cf
Richard Tillies <rtillies@gmail.com>
2022-01-05 11:48:35 -0500

    test: added describe Iteration 1 to spec files

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,16 +2,18 @@ require './lib/exhibit'
 require './lib/patron'
 
 RSpec.describe Exhibit do
-  before :each do
-    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
-  end
+  describe 'Iteration 1' do
+    before :each do
+      @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    end
 
-  it 'exists' do
-    expect(@exhibit).to be_an(Exhibit)
-  end
+    it 'exists' do
+      expect(@exhibit).to be_an(Exhibit)
+    end
 
-  it 'has attributes' do
-    expect(@exhibit.name).to eq("Gems and Minerals")
-    expect(@exhibit.cost).to eq(0)
+    it 'has attributes' do
+      expect(@exhibit.name).to eq("Gems and Minerals")
+      expect(@exhibit.cost).to eq(0)
+    end
   end
 end

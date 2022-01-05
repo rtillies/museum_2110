b1c9976382b4c57fbc809f389bf795009a85d4b0
Richard Tillies <rtillies@gmail.com>
2022-01-05 12:13:44 -0500

    test: Museum#recommend_exhibits

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,3 +1,6 @@
+require "pry"
+require "patron"
+
 class Museum
   attr_reader :name, :exhibits
 
@@ -8,5 +11,18 @@ class Museum
 
   def add_exhibit(exhibit)
     @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    patron_exhibits = []
+    patron.interests.each do |interest|
+      @exhibits.each do |exhibit|
+        # binding.pry
+        if interest == exhibit.name
+          patron_exhibits << exhibit
+        end
+      end
+    end
+    patron_exhibits
   end
 end

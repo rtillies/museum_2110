b8c54a4dd34c1a0639d553e4192908ca89d5dc5a
Richard Tillies <rtillies@gmail.com>
2022-01-05 12:16:40 -0500

    Start Iteration 3

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -45,4 +45,24 @@ RSpec.describe Museum do
       expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
     end
   end
+
+  describe 'Iteration 3' do
+    before :each do
+      @dmns = Museum.new("Denver Museum of Nature and Science")
+
+      @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      @imax = Exhibit.new({name: "IMAX",cost: 15})
+
+      @dmns.add_exhibit(@gems_and_minerals)
+      @dmns.add_exhibit(@dead_sea_scrolls)
+      @dmns.add_exhibit(@imax)
+
+      # @patron_1 = Patron.new("Bob", 20)
+      # @patron_2 = Patron.new("Sally", 20)
+    end
+
+    
+  end
+
 end
